d4ff1502f17f3f888ec133ee363bc750bfbe2ca9
shturkhal <shturhal@gmail.com>
2026-05-01 16:06:45 +0300

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Ivan Shturkhal on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
